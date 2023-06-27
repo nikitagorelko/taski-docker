d05c9f14218e195ee2ac1d76debe92179c7969ae
root <root@epdoq66skem2orsgg8r9.auto.internal>
2023-06-27 20:33:37 +0000

    settings update

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['158.160.2.113', '127.0.0.1', 'localhost', 'ngorel.hopto.org']
 
 
 # Application definition
@@ -104,6 +104,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
